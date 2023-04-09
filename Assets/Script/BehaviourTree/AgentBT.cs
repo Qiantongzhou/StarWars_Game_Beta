11e10b0d7548a0210356be7ff2b298b9bff1b94b
TachyonAgnes <para.bola@qq.com>
2023-04-09 12:50:05 -0400

    MinorAdjustment

--- Assets/Script/BehaviourTree/AgentBT.cs
+++ Assets/Script/BehaviourTree/AgentBT.cs
@@ -2,11 +2,12 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class AgentBT : MonoBehaviour {
-    public Transform target;
     public float speed = 1f;
     public float turnSpeed = 1f;
+    [HideInInspector] public Transform target;
     private AStarAgent aStarAgent;
     private AgentBT btAgent;
     private SelectorNode root;
@@ -19,14 +20,17 @@ public class AgentBT : MonoBehaviour {
 
         // Build behavior tree
         root = new SelectorNode();
-        
         SequenceNode emergencyEvasionSeq = new SequenceNode();
         emergencyEvasionSeq.AddChild(new CheckMissleInAreaNode(btAgent, 30f));
         emergencyEvasionSeq.AddChild(new TacticalManeuverNode(btAgent));
+        //SequenceNode offensiveSeq = new SequenceNode();
+        //offensiveSeq.AddChild(new CheckEnemyInConeNode(btAgent, 30f));
+        //offensiveSeq.AddChild(new AstarMove2TargetNode(btAgent));
+        //offensiveSeq.AddChild(new TurnTowardsTargetNode(btAgent));
+        //offensiveSeq.AddChild(new AttackEnemyNode(btAgent));
         SequenceNode testSeq = new SequenceNode();
         testSeq.AddChild(new AstarRandomMoveNode(btAgent));
-        //testSeq.AddChild(new AstarMove2TargetNode(btAgent));
-        //testSeq.AddChild(new TurnTowardsTargetNode(btAgent));
+        
 
         root.AddChild(emergencyEvasionSeq);
         root.AddChild(testSeq);
