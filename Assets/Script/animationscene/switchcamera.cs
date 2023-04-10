83947c082976220975f07e65b2e43a167f0409cb
TachyonAgnes <para.bola@qq.com>
2023-04-10 19:56:59 -0400

    Hotfix

--- Assets/Script/animationscene/switchcamera.cs
+++ Assets/Script/animationscene/switchcamera.cs
@@ -44,7 +44,7 @@ public class switchcamera : MonoBehaviour
         nextcam();
         dialogs[1].SetActive(false);
         yield return new WaitForSeconds(5f);
-        SceneManager.LoadScene(1);
+        SceneManager.LoadScene("mainscreen");
     }
     public void nextcam()
     {
