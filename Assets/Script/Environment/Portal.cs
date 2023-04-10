a8e897480c8c2dd4769ecfbafe2c9e43e5001df1
PascalLP <pascallabellep_2000@outlook.com>
2023-04-09 23:21:20 -0400

    Added sonds to prefabs

--- Assets/Script/Environment/Portal.cs
+++ Assets/Script/Environment/Portal.cs
@@ -7,6 +7,13 @@ public class Portal : MonoBehaviour
     private Transform _ship;
     public Transform _destination;
 
+    public AudioSource _source;
+
+    private void Awake()
+    {
+        _source = GetComponent<AudioSource>();
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
@@ -20,6 +27,12 @@ public class Portal : MonoBehaviour
     }
 
     public void teleport() {
+        if(_source != null)
+        {
+            Debug.Log("Playing sound from " + _source.name);
+            _source.Play();
+        }
+
         Vector3 portalUp = _destination.transform.up;
         Vector3 shipForward = _ship.transform.forward;
         float angle = Vector3.SignedAngle(shipForward, portalUp, Vector3.Cross(portalUp, shipForward));
